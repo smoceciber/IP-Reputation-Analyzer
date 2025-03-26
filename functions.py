fb2a05de24ea85a8088a954630457d98a4ce8711
smoceciber <95094849+smoceciber@users.noreply.github.com>
2025-03-26 10:34:51 +0100

    add comments

--- functions.py
+++ functions.py
@@ -5,15 +5,19 @@ from OTXv2 import OTXv2
 from OTXv2 import IndicatorTypes
 from datetime import datetime
 
+# config file definition
 config_file = "ip_analyzer.conf"
 config = configparser.ConfigParser()
 config.read(config_file)
 
+# variable definition
 vt_header = json.loads(config.get("vt", "vt_header"))
 abuseipdb_header = json.loads(config.get("abuseipdb", "abuseipdb_header"))
 otx_key = config.get("otx", "key")
 otx = OTXv2(otx_key)
+
 ip=""
+
 def check_vt(ip):
     r = requests.get(
         url="https://www.virustotal.com/api/v3/ip_addresses/%s" % (str(ip)),
@@ -21,6 +25,7 @@ def check_vt(ip):
     )
     data = r.json()
 
+    # parsing data from json to variables
     ip_owner = str(data["data"]["attributes"].get("as_owner", "N/A"))
     ip_asn = str(data["data"]["attributes"].get("asn", "N/A"))
     ip_network = str(data["data"]["attributes"].get("network", "N/A"))
@@ -31,7 +36,7 @@ def check_vt(ip):
     ip_reputation = str(data["data"]["attributes"].get("reputation", "N/A"))
     ip_url = "https://www.virustotal.com/gui/ip-address/%s" % (str(ip))
 
-
+    # writing variables to report
     with open("report.txt", "a") as report:
         report.write("\nVirusTotal analysis for IP %s\n" % (str(ip)))
         report.write("IP Owner: %s\n" % (str(ip_owner)))
@@ -55,6 +60,7 @@ def check_abuseipdb(ip):
 )
     data = r.json()
 
+    # parsing data from json to variables
     ip_country = str(data["data"].get("countryName", "N/A"))
     ip_isp = str(data["data"].get("isp", "N/A"))
     ip_domain = str(data["data"].get("domain", "N/A"))
@@ -63,6 +69,7 @@ def check_abuseipdb(ip):
     ip_whitelisted = str(data["data"].get("isWhitelisted", "N/A"))
     ip_url = "https://www.abuseipdb.com/check/%s" % (str(ip))
 
+    # writing variables to report
     with open("report.txt", "a") as report:
         report.write("\nAbuseIPDB analysis for IP %s\n" % (str(ip)))
         report.write("IP Owner: %s\n" % (str(ip_isp)))
@@ -77,20 +84,28 @@ def check_abuseipdb(ip):
 
 def check_alienvault(ip):
     data = otx.get_indicator_details_full(IndicatorTypes.IPv4, ip)
+
+    # get the number of pulses
     no_pulses = len(data["general"]["pulse_info"]["pulses"])
+    # pulse counter
     i = 0 
+
+
     with open("report.txt", "a") as report:
+        # check if the ip has pulses
         if no_pulses == 0:
             report.write("\nNo pulses found for IP " + ip + "\n")
-            
         else:
             report.write("\nAlienVault pulses for IP %s\n" % (str(ip)))
             while i < no_pulses:
+                # parsing data from json to variables
                 created = (str(data["general"]["pulse_info"]["pulses"][i]["created"]))
+                # creating a human readable date
                 created_obj = datetime.strptime(created, "%Y-%m-%dT%H:%M:%S.%f")
                 pulse_date = created_obj.strftime("%d-%m-%Y %H:%M:%S")
                 pulse_name = str(data["general"]["pulse_info"]["pulses"][i]["name"])
 
+                # writing variables to report
                 report.write("Pulse: %s\n" % (str(pulse_name)))
                 report.write("Created on: %s\n" % (str(pulse_date)))
                 i += 1
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import functions
 
+# emptying report file
 open('report.txt', 'w').close()
 
+# read the ips
 with open("ips.txt", "r") as file:
     ips = [line.strip() for line in file]
 
+# for each ip in ips file, execute checkers
 for ip in ips:
     functions.check_vt(ip)
     functions.check_alienvault(ip)
